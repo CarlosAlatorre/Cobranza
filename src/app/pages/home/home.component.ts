7fdef3eccb4348c38608c4b99b8ce0c83dbe70dd
Paola Gama <paolaggd@gamil.com>
2017-11-21 11:05:18 -0700

    -Crear lista "deudores" en firebase -Obtener a todos los deudores desde firebase y enlistarlos en la tabla

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AngularFireDatabase, FirebaseListObservable} from "angularfire2/database";
+import {Observable} from "rxjs/Observable";
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  deudores:FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private db: AngularFireDatabase) {
+
+  }
 
   ngOnInit() {
+      this.deudores=this.db.list('deudores');
+
+
+      console.log(this.deudores)
   }
 
   enviarComentarioo(){

b4073620a026cd553094f1fa764383abfc67ba58
juanbracamontes <juanbracamontes02@gmail.com>
2017-11-22 12:04:58 -0700

    Agregar Abono Component

---
+++ src/app/modals/agregar-abono/agregar-abono.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-agregar-abono',
+  templateUrl: './agregar-abono.component.html',
+  styleUrls: ['./agregar-abono.component.scss']
+})
+export class AgregarAbonoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

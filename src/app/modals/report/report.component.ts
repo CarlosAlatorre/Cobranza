81be8ed26a3fef0c66b9509165d9a0d9e5aa32e6
Carlos Alatorre <carlos_alatorre_95@hotmail.com>
2017-11-25 00:39:11 -0700

    Correccion de bugs y añadiendo perfect scroll

--- src/app/modals/report/report.component.ts
+++ src/app/modals/report/report.component.ts
@@ -1,25 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
 import {AngularFireDatabase, FirebaseListObservable} from "angularfire2/database";
 import * as jsPDF from 'jspdf';
-import { Angular2Csv } from 'angular2-csv/Angular2-csv';
+import {Angular2Csv} from 'angular2-csv/Angular2-csv';
 
 @Component({
-  selector: 'app-report',
-  templateUrl: './report.component.html',
-  styleUrls: ['./report.component.scss']
+    selector: 'app-report',
+    templateUrl: './report.component.html',
+    styleUrls: ['./report.component.scss']
 })
 export class ReportComponent implements OnInit {
-    deudores:FirebaseListObservable<any[]>;
-    deudor:any[]=[];
-    length:any;
-    date:any = new Date();
-    deudorTemporal:any[]=[];
-    arrayDeudores:any[]=[];
+    deudores: FirebaseListObservable<any[]>;
+    deudor: any[] = [];
+    length: any;
+    date: any = new Date();
+    deudorTemporal: any[] = [];
+    arrayDeudores: any[] = [];
 
-  constructor(private activeModal: NgbActiveModal,
-              private db: AngularFireDatabase) { }
-    options:any = {
+    constructor(private activeModal: NgbActiveModal,
+                private db: AngularFireDatabase) {
+    }
+
+    options: any = {
         fieldSeparator: ',',
         quoteStrings: '"',
         decimalseparator: '.',
@@ -28,38 +30,38 @@ export class ReportComponent implements OnInit {
         useBom: true
     };
 
-  encabezado:any ={
-      Nombre:'Nombre',
-      Deuda:'Deuda',
-      Vencimiento:'Vencimiento',
-      Direccion:'Direccion',
-      Superficie:'Superficie'
-  }
+    encabezado: any = {
+        Nombre: 'Nombre',
+        Direccion: 'Direccion',
+        Superficie: 'Superficie',
+        Deuda: 'Deuda',
+        Vencimiento: 'Vencimiento'
 
+    }
 
+    ngOnInit() {
+        this.deudores = this.db.list('deudores');
+        this.deudores.subscribe((result: any) => {
+            this.arrayDeudores.push(this.encabezado);
+            for (let i = 0; i <= result.length - 1; i++) {
+                if (result[i].estado == "deuda" || result[i].estado == "Deuda") {
+                    this.deudorTemporal.push(result[i]);
 
-  ngOnInit() {
-      this.deudores=this.db.list('deudores');
-      this.deudores.subscribe((result:any)=>{
-          this.arrayDeudores.push(this.encabezado);
-        for(let i = 0; i <= result.length - 1; i++){
-            if(result[i].estado=="deuda"||result[i].estado =="Deuda"){
-                this.deudorTemporal.push(result[i]);
-
-                this.arrayDeudores.push({
-                    nombre: result[i].nombre,
-                    totalDeuda: "$" +  result[i].totalDeuda,
-                    vencimiento: result[i].vencimiento,
-                    direccion: result[i].domicilio,
-                    superficie: result[i].superficie});
+                    this.arrayDeudores.push(
+                        {
+                            nombre: result[i].nombre,
+                            direccion: result[i].domicilio,
+                            superficie: result[i].superficie,
+                            totalDeuda: "$" + result[i].totalDeuda,
+                            vencimiento: result[i].vencimiento
+                        });
+                }
             }
+        })
+    }
 
-        }
-      })
-  }
-
-    generarCsv(){
-        new Angular2Csv(this.arrayDeudores,"Reporte del dia "+(this.date.getUTCDate()-1)+"/"+(this.date.getMonth()+1)+"/"+this.date.getFullYear())
+    generarCsv() {
+        new Angular2Csv(this.arrayDeudores, "Reporte del dia " + (this.date.getUTCDate()) + "/" + (this.date.getMonth() + 1) + "/" + this.date.getFullYear())
     }
 
 
@@ -67,7 +69,7 @@ export class ReportComponent implements OnInit {
         this.activeModal.dismiss();
     }
 
-    generatePDF(){
+    generatePDF() {
         const elementToPrint = document.getElementById('tablaHistorial'); //The html element to become a pdf
         const pdf = new jsPDF('p', 'pt', 'a4');
         pdf.addHTML(elementToPrint, () => {
